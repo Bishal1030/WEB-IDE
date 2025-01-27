de748ca28c5d543396eec9eb728bb852868238d6
Bishal1030 <bishal4shahi@gmail.com>
2025-01-27 20:00:13 +0545

    backend folder section created

--- server/index.js
+++ server/index.js
@@ -1,16 +1,23 @@
 const express = require('express')
 const http = require('http');
+const fs = require('fs/promises');
 const { Server : socketServer } = require('socket.io');
-var pty = require('node-pty');
+const pty = require('node-pty');
+const path = require('path');
 
 const app = express() 
 const server = http.createServer(app);
+
+app.get('/files', async(req,res) => {
+    const fileTree = await generateFileTree('./user')
+    return res.json({tree: fileTree})
+})
 
 const ptyProcess = pty.spawn('powershell.exe', ['-NoLogo', '-NoProfile'], {
     name: 'xterm-color',
     cols: 80,
     rows: 30,
-    cwd: process.env.INIT_CWD || process.cwd() + './user',
+    cwd: process.env.INIT_CWD || path.join(process.cwd(), 'user'),
     env: process.env
 });
 
@@ -40,3 +47,23 @@ io.on('connection', (socket) => {
 server.listen(9000, () => {
   console.log('Server is up and running on port 9000')
 });
+
+async function generateFileTree(directory) {
+    const tree = {}
+
+    async function buildTree(currentDir, currentTree){
+        const files = await fs.readdir(currentDir);
+        for(const file of files){
+            const filePath = path.join(currentDir, file);
+            const stat = await fs.stat(filePath); // Await the fs.stat call
+            if(stat.isDirectory()){
+                currentTree[file] = {};
+                await buildTree(filePath, currentTree[file]);
+            } else {
+                currentTree[file] = null;
+            }
+        }
+    }
+    await buildTree(directory, tree);
+    return tree;
+}
